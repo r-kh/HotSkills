4b044473f9de9bf27847a55ac218c68df0bc9dc0
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2025-06-29 15:21:02 +0300

    refactor(scripts.js): вынес генерацию даты и времени в общие переменные

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,19 +7,24 @@ document.addEventListener('DOMContentLoaded', function () {
     /* Этот код выполнится, когда HTML будет загружен и готов к работе
        Если удалить - весь скрипт может выполниться до загрузки DOM-элементов и сломаться. */
 
+    // Получаем текущие дату и время
+    const now = new Date();
+    // Форматируем дату в YYYY.MM.DD
+    const date = `${now.getFullYear()}.${String(now.getMonth() + 1).padStart(2, '0')}.${String(now.getDate()).padStart(2, '0')}`;
+    // Получаем локализованное время
+    const time = now.toLocaleTimeString();
+    // Получаем таймзону
+    const tz = now.toLocaleTimeString(undefined, {timeZoneName: 'short'}).split(' ').pop();
+
 
     // --- Локализованная текущая дата --- //
-    // Получаем ссылку на HTML-элемент с id "current-date"
-    document.getElementById('current-date').textContent =
-        // Получаем текущую дату, и отображаем её в этом HTML-элементе в локализованном формате
-        (d => `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, '0')}.${String(d.getDate()).padStart(2, '0')}`)(new Date());// (формат YYYY.MM.DD)
+    // Получаем ссылку на HTML-элемент с id "current-date" и отображаем текущую дату в локализованном формате
+    document.getElementById('current-date').textContent = date;
 
 
     // --- Локализованные текущие дата и время --- //
-    // Получаем ссылку на HTML-элемент с id "current-datetime"
-    document.getElementById('current-datetime').textContent =
-        // Получаем текущие дату и время, и отображаем их в этом HTML-элементе в локализованном формате
-        new Date().toLocaleString(undefined, {timeZoneName: 'short'}); // (включая короткое название часового пояса (например, GMT+3))
+    // Получаем ссылку на HTML-элемент с id "current-datetime" и отображаем текущие дату и время в этом HTML-элементе в локализованном формате
+    document.getElementById('current-datetime').textContent = `${date}, ${time} ${tz}`;
 
 
     // Запуск графиков
