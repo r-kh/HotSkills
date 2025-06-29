b9bbad5460c2d749422bca818bd8357fecfcc684
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2025-06-29 14:36:14 +0300

    feat: добавлен вывод локализованной текущей даты в заголовок таблицы зарплат

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,7 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
     /* Этот код выполнится, когда HTML будет загружен и готов к работе
        Если удалить - весь скрипт может выполниться до загрузки DOM-элементов и сломаться. */
 
-    // --- Локализованное текущее дата и время --- //
+
+    // --- Локализованная текущая дата --- //
+    // Получаем ссылку на HTML-элемент с id "current-date"
+    document.getElementById('current-date').textContent =
+        // Получаем текущую дату, и отображаем её в этом HTML-элементе в локализованном формате
+        (d => `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, '0')}.${String(d.getDate()).padStart(2, '0')}`)(new Date());// (формат YYYY.MM.DD)
+
+
+    // --- Локализованные текущие дата и время --- //
     // Получаем ссылку на HTML-элемент с id "current-datetime"
     document.getElementById('current-datetime').textContent =
         // Получаем текущие дату и время, и отображаем их в этом HTML-элементе в локализованном формате
