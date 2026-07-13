ddafdf9daaab7c50bb9d7634457b575b130282dd
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2026-07-13 19:51:10 +0300

    feat(vacancies): рефакторинг

--- app/api/vacancies.py
+++ app/api/vacancies.py
@@ -7,6 +7,7 @@ import logging
 
 # --- Сторонние библиотеки ---
 import re
+import copy
 from fastapi import APIRouter, Request       # Маршрутизатор и запросы к FastAPI
 from fastapi.responses import JSONResponse   # Ответы в формате JSON (для API)
 from asyncpg.exceptions import PostgresError
@@ -37,61 +38,87 @@ async def get_vacancies(request: Request, search: str | None = None):
     """
 
     # Получаем доступ к пулам соединений с БД и Redis
-    db_pool    = request.app.state.db_pool
-    redis_pool = request.app.state.redis_pool
+    postgresql = request.app.state.db_pool
+    redis      = request.app.state.redis_pool
 
-    # --- Пробуем достать кэш ---
-    cache_key = "vacancies"
-    cached    = await get_cache(redis_pool, cache_key)
 
-    if cached:
-        logging.info("Берём вакансии из кеша")
-        result = cached
-
+    # --- Формируем имя ключа по которому будем искать данные в Redis ---
+    if search:
+        имя_ключа_кэш_данных = f"vacancies:{search.strip().lower()}"
     else:
+        имя_ключа_кэш_данных = "vacancies:no_description"
+
+
+
+    # --- Пробуем достать кэш по этому ключу ---
+    данные_с_кэша = await get_cache(redis, имя_ключа_кэш_данных)
+
+    if данные_с_кэша is not None:
+        logging.info("Берём вакансии из кэша")
+        return JSONResponse(content=данные_с_кэша)
+
+
+
+    # --- Если кэша нет - то нужно взять все вакансии с параметрами из Redis если есть, или из БД (и заодно закешировать их в Redis), выполнить фильтрацию, закешировать и вернуть пользователю
+    # проверяем закэшированы ли вакансии с description
+
+    закэшированые_вакансии_с_description = await get_cache(redis,"vacancies")
+
+    # если не кэшированы идем в PostgreSQL за ними
+    if закэшированые_вакансии_с_description is None:
         try:
-            async with db_pool.acquire() as conn:
+            async with postgresql.acquire() as conn:
 
                 # получаем данные
                 rows = await conn.fetch("""SELECT id, name, employer, date, responses, labor_contract, salary, description FROM vacancies;""")
 
-                result = {"vacancies": [{
-                    "id"             : row["id"],
-                    "name"           : row["name"],
-                    "Работодатель"   : row["employer"],
-                    "date"           : row["date"].isoformat(),
-                    "responses"      : row["responses"],
-                    "labor_contract" : row["labor_contract"],
-                    "salary"         : row["salary"],
-                    "description"    : row["description"]}
-                    for row in rows ]}
+                # нам понадобится делать 2 кэша, 1 со всеми параметрами для поиска, 2 без description для быстрой передачи пользователю
+                вакансии_с_description = {"vacancies": []}
+                вакансии_без_description = {"vacancies": []}
+                # Проходимся по всем вакансиям
+                for row in rows:
 
-            # кэшируем на 30 минут
-            await set_cache(redis_pool, cache_key, result, expire=CACHE_TTL_30_MIN)
+                    вакансия = {
+                        "id"             : row["id"],
+                        "name"           : row["name"],
+                        "Работодатель"   : row["employer"],
+                        "date"           : row["date"].isoformat(),
+                        "responses"      : row["responses"],
+                        "labor_contract" : row["labor_contract"],
+                        "salary"         : row["salary"]}
+
+                    вакансии_без_description["vacancies"].append(вакансия)
+                    вакансии_с_description["vacancies"].append(вакансия | {"description": row["description"]})
+
+            # кэшируем полученные вакансии в Redis на 30 минут с ключом "vacancies"
+            await set_cache(redis, "vacancies", вакансии_с_description, expire=CACHE_TTL_30_MIN)
+            await set_cache(redis, "vacancies:no_description", вакансии_без_description, expire=CACHE_TTL_30_MIN)
+            # если нам изначально нужны были все вакансии без description, то можно их уже возвращать
+            if имя_ключа_кэш_данных == "vacancies:no_description":
+                return JSONResponse(content=вакансии_без_description)
 
         except PostgresError as e:
             logging.error("Ошибка при запросе к базе данных (vacancies): %s", e)
             return JSONResponse(status_code=500, content={"error": str(e)})
 
-    # -------- Поиск --------
-    if search:
-        # Поддержка запросов вида "Go OR Golang"
-        queries = [q.strip().lower() for q in search.split("OR")]
 
-        def match_text(text: str, query: str) -> bool:
-            return re.search(rf"\b{re.escape(query)}\b", text.lower()) is not None
+    # Поддержка запросов вида "Go OR Golang"
+    ключевые_слова_для_поиска = [q.strip().lower() for q in search.split("OR")]
 
-        result["vacancies"] = [
-            vacancy
-            for vacancy in result["vacancies"]
-            if any(
-                match_text(vacancy["name"] or "", query)
-                or match_text(vacancy["description"] or "", query)
-                for query in queries
-            )
-        ]
+    вакансии = закэшированые_вакансии_с_description or вакансии_с_description
+
+    def match_text(text: str, query: str) -> bool:
+        return re.search(rf"\b{re.escape(query)}\b", text.lower()) is not None
 
 
-    # -------- Убираем description перед отдачей --------
-    response = {"vacancies": [{key: value for key, value in vacancy.items() if key != "description"} for vacancy in result["vacancies"]]}
-    return JSONResponse(content=response)
+    вакансии_по_поиску_без_description = {
+        "vacancies": [
+            {key: value for key, value in vacancy.items() if key != "description"}
+            for vacancy in вакансии["vacancies"]
+            if any(match_text(vacancy["name"], ключевое_слово) or match_text(vacancy["description"], ключевое_слово)
+                   for ключевое_слово in ключевые_слова_для_поиска)]}
+
+
+    await set_cache(redis, имя_ключа_кэш_данных, вакансии_по_поиску_без_description, expire=CACHE_TTL_30_MIN)
+
+    return JSONResponse(content=вакансии_по_поиску_без_description)
